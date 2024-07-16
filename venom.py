e4ab00c70a8662d7f0559941d1ee32f3fce47741
amritpop010 <amritpop010@gmail.com>
2024-07-16 11:46:39 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7255048090:AAH2_TjHO-6Kk8WVZiwPdbZMfSPUJhwrZNY'
-ADMIN_ID = '5588464519'
+API_TOKEN = '7259843543:AAHFFnL0rhRkCmMQS4eUST6YlLOcTPFospA'
+ADMIN_ID = '1953410009'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
